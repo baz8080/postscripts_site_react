dc11e8b4e4c895242a90fa776e721b3030a9b3f2
Barry Carroll <itsmrbarry@gmail.com>
2024-04-13 16:56:08 +0100

    Support filtering by publish date

--- src/App.js
+++ src/App.js
@@ -19,12 +19,31 @@ import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 
 const connector = new ElasticsearchAPIConnector({
-  host: "https://localhost:9200",
+  host: "https://nasty.local:9200",
   apiKey: process.env.REACT_APP_ES_API_KEY,
   index: "podcasts"
 });
 
+
+
+const todayMs = Date.now();
+const todayDate = new Date(todayMs)
+
+let oneYearAgo = new Date(todayMs)
+oneYearAgo.setUTCFullYear(todayDate.getUTCFullYear() - 1)
+
+let threeYearsAgo = new Date(todayMs)
+threeYearsAgo.setUTCFullYear(todayDate.getUTCFullYear() - 3)
+
+let fiveYearsAgo = new Date(todayMs)
+fiveYearsAgo.setUTCFullYear(todayDate.getUTCFullYear() - 5)
+
+let tenYearsAgo = new Date(todayMs)
+tenYearsAgo.setUTCFullYear(todayDate.getUTCFullYear() - 10)
+
+
 const config = {
+
   searchQuery: {
     search_fields: {
       episode_title: {},
@@ -41,29 +60,39 @@ const config = {
         snippet: {size: 3200, fallback:true}
       }
     },
-    disjunctiveFacets: ["podcast_title.keyword", "podcast_type.keyword", "episode_type.keyword"],
+    disjunctiveFacets: ["podcast_title.keyword", "podcast_type.keyword", "episode_type.keyword", "episode_published_on"],
     facets: {
       "podcast_title.keyword": { type: "value" },
       "podcast_type.keyword": { type: "value" },
       "episode_type.keyword": { type: "value" },
-      // released: {
-      //   type: "range",
-      //   ranges: [
-      //     {
-      //       from: "2012-04-07T14:40:04.821Z",
-      //       name: "Within the last 10 years"
-      //     },
-      //     {
-      //       from: "1962-04-07T14:40:04.821Z",
-      //       to: "2012-04-07T14:40:04.821Z",
-      //       name: "10 - 50 years ago"
-      //     },
-      //     {
-      //       to: "1962-04-07T14:40:04.821Z",
-      //       name: "More than 50 years ago"
-      //     }
-      //   ]
-      // },
+      episode_published_on: {
+        type: "range",
+        ranges: [
+          {
+            from: oneYearAgo.toISOString(),
+            name: "Within the last 1 year"
+          },
+          {
+            from: threeYearsAgo.toISOString(),
+            to: oneYearAgo.toISOString(),
+            name: "1 - 3 years ago"
+          },
+          {
+            from: fiveYearsAgo.toISOString(),
+            to: threeYearsAgo.toISOString(),
+            name: "3 - 5 years ago"
+          },
+          {
+            from: tenYearsAgo.toISOString(),
+            to: fiveYearsAgo.toISOString(),
+            name: "5 - 10 years ago"
+          },
+          {
+            to: tenYearsAgo.toISOString(),
+            name: "More than 10 years ago"
+          }
+        ]
+      },
       episode_duration: {
         type: "range",
         ranges: [
@@ -80,6 +109,7 @@ const config = {
 };
 
 function App() {
+
   return (
     <SearchProvider config={config}>
       <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
@@ -98,6 +128,7 @@ function App() {
                       <Facet key={"2"} field={"podcast_type.keyword"} label={"pttpe"} />
                       <Facet key={"3"} field={"episode_type.keyword"} label={"etype"} />
                       <Facet key={"4"} field={"episode_duration"} label={"duration"} />
+                      <Facet key={"5"} field={"episode_published_on"} label={"published"} />
                     </div>
                   }
                   bodyContent={<Results shouldTrackClickThrough={true} />}
